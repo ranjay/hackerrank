760a34521b3630a32ffc8c3bdf968d25da540b9d
ranjay <ranjay007@gmail.com>
2018-02-09 13:37:01 -0500

    fix

--- maxsubarray.js
+++ maxsubarray.js
@@ -13,18 +13,40 @@ function maximumSum(a, m) {
 
 
     function subar(subarraysize, sasize) {
-        if (subarraysize >= 0) {
-            for (i = 0; i < subarraysize; i++) {
-                s1 = a.slice(i, sasize + i)
-                console.log(s1)
-                if (s1.length > 0) {
-                    sm = sum(s1);
-                    s3 = sm % m
+        // console.log(subarraysize)
+        // console.log(max)
 
-                    if (s3 > max) {
-                        max = s3
+        if (subarraysize >0) {
+            if (sasize == 1) {
+                a1 = a.map(x => x % m); 
+                m1 = Math.max(...a1)
+                if (m1 > max) {
+                    max = m1
+                }
+
+            } else if (sasize ==  a.length) {
+                s2 = sum(a);
+                s4 = s2 % m
+                if (s4 > max) {
+                    max = s4
+                }
+            } else {
+                for (i = 0; i < subarraysize; i++) {
+                    s1 = a.slice(i, sasize + i)
+                   
+                    if (s1.length > 0) {
+                        sm = sum(s1);
+                       
+                        s3 = sm % m
+                       
+                        if (s3 > max) {
+                          
+                            max = s3
+                          
+                        }
                     }
                 }
+
             }
             subarraysize = subarraysize - 1
             sasize = sasize + 1
